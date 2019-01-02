b614a830c637471f31dd68fcbf99c88ceee50209
sceptreCnsn <cansin.memis@clb.lcl>
2019-01-02 18:28:54 +0300

    second

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = ({videos}) => {
         return <VideoItem video={video}/>
     });
     return(
-        <div>{renderedList}</div>
+        <div className="ui relaxed divided list">{renderedList}</div>
     );
 }
 
